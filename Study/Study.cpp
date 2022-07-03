17c6609bafcc1db32233eb7bfe52a36fa4840010
Alexander <Alexander@LAPTOP-S3JMF2AH>
2022-07-03 23:00:33 +0300

    Добавьте файлы проекта.

---
+++ Study/Study.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void func(int a[])
+{
+	*(a - 1) = 10;
+	a++;
+	*a = 20;
+	a[-3] = 30;
+}
+int main()
+{
+	int c, state = OUT, n = 0;
+	while ((c = getchar()) != '\n')
+	{
+		if (isdigit(c))
+		{
+			++n;
+			state = IN;
+		}
+		else
+			state = OUT;
+	}
+	printf("%d", n);
+}
